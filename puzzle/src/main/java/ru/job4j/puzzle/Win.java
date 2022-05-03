d85a5063fd660efcb3270ba55b76b051d9fedcff
Tamerlanaz <tamerlanaz@gmail.com>
2022-05-03 18:45:12 +0300

    2. Сокобан. Тестовое задание. [#75283 #296548]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -6,13 +6,12 @@ public class Win {
         for (int index = 0; index < board.length; index++) {
             if (board[index][index] == 1 && (horizontal(board, index) || vertical(board, index))) {
                 rsl = true;
-                break;
             }
         }
         return rsl;
     }
 
-    public static boolean horizontal(int[][] board, int row) {
+    public static boolean horizontal(int [][] board, int row) {
         boolean result = true;
         for (int i = 0; i < board.length; i++) {
             if (board[row][i] != 1) {
@@ -33,6 +32,5 @@ public class Win {
         }
         return result;
     }
-
-
 }
+
